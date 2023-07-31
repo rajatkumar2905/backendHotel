365836fb1399ba7f0d31d8b93c8de5b3103c8107
RaJat29Kumar <kumar.rj295@gmail.com>
2023-07-31 17:20:12 +0530

    fixed redirect

--- Routers/user.js
+++ Routers/user.js
@@ -15,7 +15,7 @@ router.get(
 router.get(
   "/login",
   passport.authenticate("google", {
-    successRedirect: process.env.FRONTEND_URL,
+    successRedirect:process.env.FRONTEND_URL,
   })
 );
 
