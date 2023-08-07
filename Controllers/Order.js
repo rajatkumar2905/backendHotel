0bd118cd0f2c68a882eb7e253fed194833b280fd
RaJat29Kumar <kumar.rj295@gmail.com>
2023-08-07 23:40:29 +0530

    fixed redirect

--- Controllers/Order.js
+++ Controllers/Order.js
@@ -13,7 +13,8 @@ export const placeOrder = asyncError(async (req, res, next) => {
       totalAmount,
     } = req.body;
   
- 
+    const user = req.user._id;
+
     const orderOptions = {
       shippingInfo,
       orderItems,
@@ -22,7 +23,7 @@ export const placeOrder = asyncError(async (req, res, next) => {
       taxPrice,
       shippingCharges,
       totalAmount,
-      user: req.user._id,
+      user,
       
     };
     await Order.create(orderOptions);
